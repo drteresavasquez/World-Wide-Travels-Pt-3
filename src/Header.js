1eaf4bfae596da521f5685b6749445842b4648a9
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-04-08 10:41:03 -0500

    updated ui so components display properly on mobile

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(){
     return(
     <header className="App-header">
-      <h1 className="ui dividing centered header">Weather Dashboard</h1>
+      <h1 className="ui centered header">Weather Dashboard</h1>
     </header>
     )
 }
